d912da96281e7a384f7bf02c26b67552538f9c17
johnrevans6 <johnre@hawaii.edu>
2016-02-20 01:40:58 -0800

    minor fixes to clean up initial routes

--- app/module.js
+++ app/module.js
@@ -2,11 +2,11 @@ angular.module('healthTrakApp',['ngRoute','ngResource'])
     .config(['$routeProvider', '$locationProvider',function($routeProvider,$locationProvider){
         $routeProvider.when('/',{
             controller: 'indexCtrl',
-            templateUrl: '/assets/partials/index.html'
+            templateUrl: '/app/partials/index.html'
         })
         .when('/about',{
             controller: 'aboutCtrl',
-            templateUrl: '/assets/partials/about.html'
+            templateUrl: '/app/partials/about.html'
         })
         .otherwise({
             redirectTo: '/'
